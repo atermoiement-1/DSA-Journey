06cfac084c476dedb63ed6a7602eba9cdddbd081
VishnuVardhan Sathani <vishnuvardhan6840@gmail.com>
2026-06-19 18:40:19 +0530

    Refactor variable name for clarity in twoSum method

--- Arrays/two_sum.py
+++ Arrays/two_sum.py
@@ -25,9 +25,9 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         dict = {}
         for i in range(len(nums)):
-            compliment = target - nums[i]
-            if compliment in dict:
-                return [dict[compliment], i]
+            value = target - nums[i]
+            if value in dict:
+                return [dict[value], i]
             dict[nums[i]] = i
         return []
 
